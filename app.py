5b337467d4dfd64861ebdbb3da76cad6ecf85485
Victor (VicMCA) <victor.mcavalcanti@gmail.com>
2021-12-02 18:32:26 -0300

    codigo sendo exibido na pagina e atualizavel, mas ainda com bug devido ao cache do navegador

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route("/", methods=["POST", "GET"])
 def index():
